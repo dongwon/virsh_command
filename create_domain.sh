cf295bd3dfd2b8802d359b44a18dc070ef706f2d
won <won@won.(none)>
2013-12-07 01:21:51 +0900

    recommit

--- create_domain.sh
+++ create_domain.sh
@@ -1,43 +1,62 @@
 #!/bin/bash
-POOL=kvm
 DOMAIN_NAME=$1
 CPUS=1
 RAM=1024
 DISK_SIZE=8
 FORMAT=qcow2
 
+. ./hosts
+
 #OS_VARIANT=rhel5.4
-#LOCATION="http://192.168.8.3/mirrors/centos/5/os/x86_64/"
-#LOCATION="http://192.168.8.3/mirrors/rhel/5/"
-#KS=http://192.168.8.3/ks/centos5.cfg
-#KS=http://192.168.8.3/ks/rhel5.cfg
+#LOCATION="http://repo.donga.ktc/mirrors/centos/5/os/x86_64/"
+#LOCATION="http://repo.donga.ktc/mirrors/rhel/5/"
+#KS=http://repo.donga.ktc/ks/centos5.cfg
+#KS=http://repo.donga.ktc/ks/rhel5.cfg
 
 #OS_VARIANT=rhel6
-#LOCATION="http://192.168.8.3/mirrors/centos/6/os/x86_64/"
-#LOCATION="http://192.168.8.3/mirrors/rhel/6/"
-#KS=http://192.168.8.3/ks/centos6.cfg
-#KS=http://192.168.8.3/ks/rhel6.cfg
+#LOCATION="http://repo.donga.ktc/mirrors/centos/6/os/x86_64/"
+#LOCATION="http://repo.donga.ktc/mirrors/rhel/6/"
+#KS=http://repo.donga.ktc/ks/centos6.cfg
+#KS=http://repo.donga.ktc/ks/rhel6.cfg
 
 OS_VARIANT=ubuntuprecise
-LOCATION="http://192.168.8.3/mirrors/ubuntu/dists/precise/main/installer-amd64/"
-KS=http://192.168.8.3/ks/ubuntu.cfg
+LOCATION="http://repo.donga.ktc/mirrors/ubuntu/dists/precise/main/installer-amd64/"
+KS=http://repo.donga.ktc/ks/ubuntu.cfg
 
 BR1=br-ext
 BR2=br-int
 BR3=br-sec
 BR4=br-isol
 
-virsh vol-create-as $POOL $DOMAIN_NAME "$DISK_SIZE"G --allocation "$DISK_SIZE"G --format $FORMAT
-VOL_PATH=$(virsh vol-list --pool $POOL | grep $DOMAIN_NAME | awk '{print $2}')
+virsh --connect $URL vol-create-as $POOL $DOMAIN_NAME "$DISK_SIZE"G --allocation "$DISK_SIZE"G --format $FORMAT
+VOL_PATH=$(virsh --connect $URL vol-list --pool $POOL | grep $DOMAIN_NAME | awk '{print $2}')
 
+## Installation using PXE
+#virt-install \
+#--connect $URL \
+#--virt-type kvm \
+#--name $DOMAIN_NAME \
+#--ram $RAM \
+#--vcpus=$CPUS \
+#--disk=$VOL_PATH,size=$DISK_SIZE,bus=virtio,cache=writeback,sparse=true,format=$FORMAT \
+#--network bridge=$BR1,model=virtio \
+#--os-variant $OS_VARIANT \
+#--vnc \
+#--pxe \
+
+## Installation using local location
 virt-install \
---connect qemu:///system \
+--connect $URL \
 --virt-type kvm \
 --name $DOMAIN_NAME \
 --ram $RAM \
 --vcpus=$CPUS \
 --disk=$VOL_PATH,size=$DISK_SIZE,bus=virtio,cache=writeback,sparse=true,format=$FORMAT \
---network bridge=$BR2,model=virtio \
+--network bridge=$BR1,model=virtio \
 --os-variant $OS_VARIANT \
 --location=$LOCATION \
+--graphics none \
 --extra-args "ks=$KS ksdevice=eth0 console=tty0 console=ttyS0,115200"
+
+
+
--- destroy_domain.sh
+++ destroy_domain.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-virsh snapshot-delete $1 default
-virsh destroy $1
-virsh undefine $1
-virsh vol-delete $1 kvm
+. ./hosts
+
+virsh --connect $URL snapshot-delete $1 default
+virsh --connect $URL destroy $1
+virsh --connect $URL undefine $1
+virsh --connect $URL vol-delete $1 --pool $POOL
